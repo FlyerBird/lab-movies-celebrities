ae74331cb40e7eff72163bfa626cb0ee04a3cb13
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-06-11 19:06:40 +0200

    iteration2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,21 @@
 //  Add your code here
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const celebritySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  occupation: {
+    type: String,
+    required: true
+  },
+  catchPhrase: {
+    type: String 
+  }
+})
+
+const Season = mongoose.model('Celebrity.model', celebritySchema);
+
+module.exports = Celebrity.model;
